02f39113faabf14af8b4fa9132ee582b401f84b5
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-11-20 09:29:10 -0500

    submit form

--- 08-controls-and-user-input/starter/Sources/App/Welcome/RegisterView.swift
+++ 08-controls-and-user-input/starter/Sources/App/Welcome/RegisterView.swift
@@ -5,7 +5,7 @@
 import SwiftUI
 
 struct RegisterView: View {
-    @State var name: String = ""
+    @EnvironmentObject var user: UserManager
     @ObservedObject var keyboardHandler: KeyboardFollower
     init(keyboardHandler: KeyboardFollower) {
         self.keyboardHandler = keyboardHandler
@@ -14,9 +14,21 @@ struct RegisterView: View {
     var body: some View {
             VStack(content: {
                 WelcomeMessageView()
-                TextField("How would I call you ?", text: $name)
+                TextField("How would I call you ?", text: $user.profile.name)
                     .bordered()
-            })
+                Button(action: self.registerUser) {
+                    HStack {
+                        Image(systemName: "checkmark")
+                        .resizable()
+                            .frame(width: 16, height: 16, alignment: .center)
+                        Text("Sign up !")
+                            .font(.body)
+                        .bold()
+                    }
+                }
+            .bordered()
+                }
+            )
         .background(WelcomeBackgroundImage())
                 .padding(.bottom, keyboardHandler.keyboardHeight)
     
@@ -25,7 +37,14 @@ struct RegisterView: View {
 }
 
 struct RegisterView_Previews: PreviewProvider {
+static let user = UserManager(name: "Tim")
     static var previews: some View {
-        RegisterView(keyboardHandler: KeyboardFollower())
+        RegisterView(keyboardHandler: KeyboardFollower()).environmentObject(user)
+    }
+}
+
+extension RegisterView {
+    func registerUser() -> Void {
+        user.persistProfile()
     }
 }
